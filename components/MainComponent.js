7d36c905ef905577c5bcbccecf48bda34700d4c7
Ayush <ayushsaun@gmail.com>
2020-06-08 00:42:11 +0530

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,8 +1,9 @@
 import React , { Component } from 'react'
+import Home from './HomeComponent'
 import Menu from './MenuComponent'
 import Dishdetail from './DishdetailComponent'
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator , createDrawerNavigator } from 'react-navigation';
 
 const MenuNavigator = createStackNavigator({
         Menu: { screen: Menu },
@@ -23,6 +24,42 @@ const MenuNavigator = createStackNavigator({
 ); 
 // when navigationOptions is applied here it is applicable to all screens
 // mentioned above so if we have common config for screens we can specify them init there
+const HomeNavigator = createStackNavigator({
+        Home: { screen: Home }
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: "#fff"            
+            }
+        }
+    }
+); 
+
+const MainNavigator = createDrawerNavigator({
+        Home: {
+            screen: HomeNavigator,
+            navigationOptions: {
+                title: 'Home',
+                drawerLabel: 'Home'
+            }
+        },
+        Menu: {
+            screen: MenuNavigator,
+            navigationOptions: {
+                title: 'Menu',
+                drawerLabel: 'Menu'
+            }
+        }
+    },
+    {
+        drawerBackgroundColor: '#D1C4E9'
+    }
+)
 
 class Main extends Component {
     
@@ -31,7 +68,7 @@ class Main extends Component {
             // by using Expo.Constants.statusBarHeight we will get enough space on the top for status bar to be displayed
             // Expo.Constants.statusBarHeight will show error on web after compilation so just observe that on mobile app instead
             <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-                <MenuNavigator />
+                <MainNavigator />
             </View>
         )
     }
