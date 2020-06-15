a633d75d37ec61be1d0f05f6d93365e60d7291b3
Ayush <ayushsaun@gmail.com>
2020-06-15 16:41:06 +0530

    Persist Redux Store

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Main from './components/MainComponent'
 import { Provider } from 'react-redux'
 import { ConfigureStore } from './redux/configureStore'
+import { PersistGate } from 'redux-persist/es/integration/react'
+import { Loading } from './components/LoadingComponent'
 
-const store = ConfigureStore();
+const { persistor , store } = ConfigureStore();
 
 // const instructions = Platform.select({
 //   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
@@ -14,7 +16,12 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Main />
+        <PersistGate 
+          loading = {<Loading />}
+          persistor={persistor}
+        >
+          <Main />
+        </PersistGate>
       </Provider>      
     );  
   }
