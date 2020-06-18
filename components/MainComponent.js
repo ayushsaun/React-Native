67d899af43f8b49c613a31ce29203820c6a78d23
Ayush <ayushsaun@gmail.com>
2020-06-18 20:54:47 +0530

    Network Info

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -7,7 +7,8 @@ import Dishdetail from './DishdetailComponent'
 import Reservation from './ReservationComponent'
 import Favorites from './FavoriteComponent'
 import Login from './LoginComponent'
-import { View , Platform , Image , StyleSheet , ScrollView , Text } from 'react-native';
+import { View , Platform , Image , StyleSheet , ScrollView , Text , ToastAndroid } from 'react-native';
+import NetInfo from "@react-native-community/netinfo";
 import { createStackNavigator , createDrawerNavigator , DrawerItems , SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements'
 import { connect } from 'react-redux'
@@ -306,8 +307,40 @@ class Main extends Component {
         this.props.fetchComments();
         this.props.fetchLeaders();
         this.props.fetchPromos();
+
+        NetInfo.fetch()
+        .then((connectionInfo) => {
+            ToastAndroid.show('Initial Network Connectivity Type: '
+                + connectionInfo.type + ', effectiveType: ' + connectionInfo.effectiveType,
+                ToastAndroid.LONG)
+        });
+
+        window.value=NetInfo.addEventListener((connectionInfo) => this.handleConnectivityChange(connectionInfo)) ;
+    }
+
+    componentWillUnmount(){
+        window.value();
     }
     
+    handleConnectivityChange = (connectionInfo) => {
+        switch (connectionInfo.type) {
+          case 'none':
+            ToastAndroid.show('You are now offline!', ToastAndroid.LONG);
+            break;
+          case 'wifi':
+            ToastAndroid.show('You are now connected to WiFi!', ToastAndroid.LONG);
+            break;
+          case 'cellular':
+            ToastAndroid.show('You are now connected to Cellular!', ToastAndroid.LONG);
+            break;
+          case 'unknown':
+            ToastAndroid.show('You now have unknown connection!', ToastAndroid.LONG);
+            break;
+          default:
+            break;
+        }
+      }
+
     render() {
         return (
             // by using Expo.Constants.statusBarHeight we will get enough space on the top for status bar to be displayed
